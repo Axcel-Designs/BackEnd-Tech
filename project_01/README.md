c1a09902710c64506f0e8dcfaf02a78adea34561
excel <axceldesigns@gmail.com>
2026-05-25 09:48:55 +0100

    01

---
+++ project_01/README.md
@@ -0,0 +1,9 @@
+# Arrays
+
+Create an array called bunnies that contains the names of six bunnies.
+
+- Add a new bunny called Mario to the end of the array.
+
+- Remove the bunny called Lucy from the array.
+
+- Add a new bunny called Luigi to the beginning of the array.
--- project_01/app.js
+++ project_01/app.js
@@ -1,4 +1,16 @@
-const bunny = { name: "Bunny", age: 5, isHappy: true };
-console.log(bunny.name); // Output: Bunny
-console.log(bunny.age); // Output: 5
-console.log(bunny.isHappy); // Output: true
+// const bunny = { name: "Bunny", age: 5, isHappy: true };
+// console.log(bunny.name); // Output: Bunny
+// console.log(bunny.age); // Output: 5
+// console.log(bunny.isHappy); // Output: true
+
+let bunnies = ["Bugs", "Lola", "Thumper", "Lucy", "Snowball", "Daisy"];
+console.log(bunnies);
+
+bunnies.push("Mario");
+console.log(bunnies);
+
+bunnies.splice(bunnies.indexOf("Lucy"), 1);
+console.log(bunnies);
+
+bunnies.unshift("Luigi");
+console.log(bunnies);
